2aaba76376620da9c867ecaf848430aae8828e8c
Ospite <ospite@imac-di-diskos14.local>
2017-06-07 17:30:01 +0200

    mio

--- TestText2Speech/ViewController.swift
+++ TestText2Speech/ViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+//buba
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    @IBOutlet weak var sldRate: UISlider!
+    @IBOutlet weak var sldPitch: UISlider!
+    @IBOutlet weak var btnSpeech: UIButton!
+    @IBOutlet weak var tvText: UITextView!
+    @IBOutlet weak var sldVolume: UISlider!
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var pvVoices: UIPickerView!
+    let speechSynth = AVSpeechSynthesizer()
+    let voices = AVSpeechSynthesisVoice.speechVoices()
+    var whoSpeaks:[String] = []
+    var myString:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pvVoices.delegate = self
+        pvVoices.dataSource = self
+        
+        var thisVoice:AVSpeechSynthesisVoice
+        for thisVoice in voices
+        {
+            myString = "\(thisVoice.name),\(thisVoice.language)"
+            whoSpeaks.append(myString)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +41,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       return whoSpeaks.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return whoSpeaks[row]
+    }
 
+    @IBAction func btnSpeech_TouchUp(_ sender: Any) {
+        let speechUtterance = AVSpeechUtterance(string:tvText.text)
+        
+        speechUtterance.rate = sldRate.value
+        speechUtterance.pitchMultiplier = sldPitch.value
+        speechUtterance.volume = sldVolume.value
+        
+        speechSynth.speak(speechUtterance)
+    }
+    
+    
 }
 
