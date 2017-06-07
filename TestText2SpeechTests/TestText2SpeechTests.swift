b4f30e168f6bd59228af3aec3b4d410e20a9b563
Ospite <ospite@imac-di-diskos14.local>
2017-06-07 15:02:46 +0200

    Initial Commit

---
+++ TestText2SpeechTests/TestText2SpeechTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestText2SpeechTests.swift
+//  TestText2SpeechTests
+//
+//  Created by Ospite on 07/06/17.
+//  Copyright © 2017 Ospite. All rights reserved.
+//
+
+import XCTest
+@testable import TestText2Speech
+
+class TestText2SpeechTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
